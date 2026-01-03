4721f2dba3bf3c139ee77a742488418d92616a5c
lugudm <lugumedeiros@gmail.com>
2026-01-03 17:35:45 -0300

    Add --verbose flag

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def get_genai_client() -> genai.Client:
 import argparse
 _parser = argparse.ArgumentParser(description="GEN-AI Chatbot")
 _parser.add_argument("user_prompt", type=str, help="User prompt")
+_parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 
 def get_parser_args() -> argparse.Namespace:
     return _parser.parse_args()
@@ -27,7 +28,7 @@ def get_parser_args() -> argparse.Namespace:
 
 MODEL = "gemini-2.5-flash"
 
-def print_genai(response:gentypes.GenerateContentResponse):
+def print_genai(response:gentypes.GenerateContentResponse, verbose=True):
     p_tokens = response.usage_metadata.prompt_token_count
     r_tokens = response.usage_metadata.candidates_token_count
     response_str = response.text
@@ -35,16 +36,18 @@ def print_genai(response:gentypes.GenerateContentResponse):
         raise RuntimeError("genai request failed, no response token used")
     output = f"Prompt tokens: {p_tokens}\n" \
         f"Response tokens: {r_tokens}\n" \
-        f"{response_str}"
-    print(output)
+        f"User prompt: {response_str}"
+    if verbose:
+        print(output)
+    else:
+        print(response_str)
 
 def main():
-    print("Hello from aiagent!")
     parser_args = get_parser_args()
     messages = [gentypes.Content(role="user", parts=[gentypes.Part(text=parser_args.user_prompt)])]
     client = get_genai_client()
     response = client.models.generate_content(model=MODEL, contents=messages)
-    print_genai(response)
+    print_genai(response, verbose=parser_args.verbose)
 
 if __name__ == "__main__":
     main()
