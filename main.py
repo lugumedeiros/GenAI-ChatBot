e3074077723fd646d642abf6c7d4a385a00e603e
lugudm <lugumedeiros@gmail.com>
2026-01-03 17:13:20 -0300

    Feature: Prompt input set as sys arg

--- main.py
+++ main.py
@@ -5,18 +5,25 @@ from google import genai
 from google.genai import types as gentypes
 
 load_dotenv()
-genai_key = os.environ.get("GEMINI_API_KEY")
-if genai_key is None:
-    raise RuntimeError("genai api key not found in env")
-
 def get_genai_key() -> str:
+    genai_key = os.environ.get("GEMINI_API_KEY")
+    if genai_key is None:
+        raise RuntimeError("genai api key not found in env")
     return genai_key
 
 def get_genai_client() -> genai.Client:
     genai_client = genai.Client(api_key=get_genai_key())
     return genai_client
 
-############################################
+# ArgParser ##########################################
+import argparse
+_parser = argparse.ArgumentParser(description="GEN-AI Chatbot")
+_parser.add_argument("user_prompt", type=str, help="User prompt")
+
+def get_parser_args() -> argparse.Namespace:
+    return _parser.parse_args()
+
+######################################################
 
 MODEL = "gemini-2.5-flash"
 
@@ -33,9 +40,11 @@ def print_genai(response:gentypes.GenerateContentResponse):
 
 def main():
     print("Hello from aiagent!")
-    request_content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    parser_args = get_parser_args()
+    print(parser_args)
+    prompt = parser_args.user_prompt
     client = get_genai_client()
-    response = client.models.generate_content(model=MODEL, contents=request_content)
+    response = client.models.generate_content(model=MODEL, contents=prompt)
     print_genai(response)
 
 if __name__ == "__main__":
